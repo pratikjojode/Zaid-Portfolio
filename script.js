e9d3477fa4e082c218ab170dfada17338df8a349
unknown <pratikjojode2004@gmail.com>
2024-01-27 15:21:59 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function openMenu() {
+  const nav2 = document.querySelector(".nav2");
+  nav2.style.display = "flex";
+}
+function closeMenu() {
+  const nav2 = document.querySelector(".nav2");
+  nav2.style.display = "none";
+}
